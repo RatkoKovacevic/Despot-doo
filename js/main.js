f4f51ee76f180b5326e0b611460f194d69164612
RatkoKovacevic <ratkokovacevic100live.de>
2018-06-26 11:51:57 +0200

    init

--- js/main.js
+++ js/main.js
@@ -13,3 +13,52 @@ $(document).ready(function () {
             $('#flipbook').turn('next');
     });
 });
+
+function initMap() {
+    let options = {
+        zoom: 14,
+        center: {
+            lat: 43.799761,
+            lng: 19.002105
+        },
+        mapTypeId: google.maps.MapTypeId.HYBRID
+    }
+    let map = new google.maps.Map(document.getElementById('map'), options);
+
+    function addMarker(coords) {
+        let marker = new google.maps.Marker({
+            position: coords,
+            map: map,
+            icon: '../img/marker.png'
+        })
+    }
+    addMarker({
+        lat: 43.797138,
+        lng: 19.004414
+    });
+    addMarker({
+        lat: 43.801279,
+        lng: 19.006433
+    });
+    addMarker({
+        lat: 43.800653,
+        lng: 19.001577
+    });
+    addMarker({
+        lat: 43.803665,
+        lng: 19.001037
+    });
+    // Vrh grada
+    addMarker({
+        lat:43.811289, 
+        lng: 18.999357
+    });
+    addMarker({
+        lat: 43.795849,
+        lng: 19.009381
+    });
+    addMarker({
+        lat: 43.887829,
+        lng: 19.109191
+    })
+};
